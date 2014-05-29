b19f40317eb374b7d384f23970fd0782390b7e74
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2014-05-29 10:20:28 -0500

    refactor and improve logging

--- mindfeed/__init__.py
+++ mindfeed/__init__.py
@@ -22,20 +22,30 @@ beeminder = BeeminderAPI(BEEMINDER_API_URL)
 datapoints = beeminder.users(USERNAME).goals(GOAL, 'datapoints.json')
 
 
-def get_new_posts():
-    print("Getting new posts")
+def get_beeminder_links():
+    print("Getting Beeminder links...")
     existing_datapoints = datapoints.GET(params={"auth_token": AUTH_TOKEN}).json()
     beeminder_links = set(point['comment'] for point in existing_datapoints)
+    return beeminder_links
+
+
+def get_feed_links():
+    print("Getting feed links...")
     feed = feedparser.parse(FEED_URL)['entries']
     feed_links = set(entry['link'] for entry in feed)
+    return feed_links
+
+
+def get_new_posts():
+    beeminder_links = get_beeminder_links()
+    feed_links = get_feed_links()
     new_posts = feed_links - beeminder_links
     return new_posts
 
 
 def create_new_datapoints(new_posts):
     for post in new_posts:
-        print("Adding datapoint for:")
-        print(post)
+        print("Adding datapoint for: {0}".format(post))
         datapoint = {
             "auth_token": AUTH_TOKEN,
             "value": 1,
@@ -55,9 +65,7 @@ def main():
         create_new_datapoints(new_posts)
         hours_remaining = 24
         while hours_remaining > 0:
-            print("Sleeping for...")
-            print(hours_remaining)
-            print("...more hours.")
+            print("Sleeping for {0} more hours.".format(hours_remaining))
             time.sleep(60 * 60)
             hours_remaining = hours_remaining - 1
 
